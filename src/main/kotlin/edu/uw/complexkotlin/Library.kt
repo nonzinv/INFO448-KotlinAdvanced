ac1f639c9f8b1969dcf6d34dcb1942bae7499891
nonzinv <nonzinv@uw.edu>
2024-01-23 08:34:23 -0800

    non work

--- src/main/kotlin/edu/uw/complexkotlin/Library.kt
+++ src/main/kotlin/edu/uw/complexkotlin/Library.kt
@@ -9,7 +9,23 @@ package edu.uw.complexkotlin
 // the final string should look like FIZZBUZZFIZZFIZZBUZZFIZZFIZZBUZZ for 0..15.
 // store this lambda into 'fizzbuzz' so that the tests can call it
 //
-val fizzbuzz : (IntRange) -> String = { "" }
+val fizzbuzz: (IntRange) -> String = { list: IntRange ->
+    list.map { number ->
+        if (number > 0) {
+            if(number % 3 == 0 && number % 5 == 0) {
+                "FIZZBUZZ"
+            } else if (number % 3 == 0) {
+                "FIZZ"
+            } else if (number % 5 == 0) {
+                "BUZZ"
+            } else {
+                ""
+            }
+        } else {
+            ""
+        }
+    }.fold("") { total, current -> total + current }
+}
 
 // Example usage
 /*
@@ -34,16 +50,27 @@ fun process(message: String, block: (String) -> String): String {
     return ">>> ${message}: {" + block(message) + "}"
 }
 // Create r1 as a lambda that calls process() with message "FOO" and a block that returns "BAR"
-val r1 = { }
+val r1 = { process("FOO") {"BAR"} }
 
 // Create r2 as a lambda that calls process() with message "FOO" and a block that upper-cases 
 // r2_message, and repeats it three times with no spaces: "WOOGAWOOGAWOOGA"
 val r2_message = "wooga"
-val r2 = { }
+val r2 = { process("FOO") {r2_message.toUpperCase().repeat(3)}}
 
 
 // write an enum-based state machine between talking and thinking
-enum class Philosopher { }
+enum class Philosopher {
+    THINKING {
+        override fun signal() = TALKING 
+        override fun toString() = "Deep thoughts...."
+    },
+    TALKING {
+        override fun signal() = THINKING
+        override fun toString() = "Allow me to suggest an idea..."
+    };
+
+    abstract fun signal(): Philosopher
+}
 
 // create an class "Command" that can be used as a function.
 // To do this, provide an "invoke()" function that takes a 
@@ -55,4 +82,23 @@ enum class Philosopher { }
 // val cmd = Command(": ")
 // val result = cmd("Hello!")
 // result should equal ": Hello!"
-class Command(val prompt: String) { }
+class Command(val prompt: String) {
+    operator fun invoke(message: String): String {
+        return prompt + message
+    }
+}
+
+fun main() {
+    println(r1())
+    println(r2())
+    var thinker = Philosopher.THINKING
+    println(thinker)
+    val cmd = Command(": ")
+    val result = cmd("Hello!")
+    println(result)
+    val result2: String = fizzbuzz(0..15)
+    println(result2)
+}
+
+
+
